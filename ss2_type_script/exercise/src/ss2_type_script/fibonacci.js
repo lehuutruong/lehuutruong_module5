28e26884f89b170b06f6af27a30441f698a362ce
TRƯỜNG KHANG <lehuutruongkhang@gmail.com>
2022-11-18 16:42:17 +0700

    C:/Program Files/Git/

--- ss2_type_script/exercise/src/ss2_type_script/fibonacci.js
+++ ss2_type_script/exercise/src/ss2_type_script/fibonacci.js
@@ -9,7 +9,7 @@ function sumFibonacci() {
         numberOne = numberTwo;
         numberTwo = numberThree;
         check++;
-        if (check > 15) {
+        if (check > 9) {
             break;
         }
     }
@@ -20,3 +20,32 @@ function sumFibonacci() {
     console.log(sum);
 }
 sumFibonacci();
+// Cach 2
+function fibonacci(n) {
+    var f0 = 0;
+    var f1 = 1;
+    var fn = 1;
+    var i;
+    if (n < 0) {
+        return -1;
+    }
+    else if (n == 0 || n == 1) {
+        return n;
+    }
+    else {
+        for (i = 2; i < n; i++) {
+            f0 = f1;
+            f1 = fn;
+            fn = f0 + f1;
+        }
+    }
+    return fn;
+}
+for (var i = 0; i < 11; i++) {
+    console.log(fibonacci(i));
+}
+var sum = 0;
+for (var i = 0; i < 10; i++) {
+    sum += fibonacci(i);
+}
+console.log(sum);
--- ss2_type_script/exercise/src/ss2_type_script/fibonacci.ts
+++ ss2_type_script/exercise/src/ss2_type_script/fibonacci.ts
@@ -1,22 +1,53 @@
 function sumFibonacci() {
-    let numberOne:number=0;
-    let numberTwo:number=1;
-    let arr:number[]=[0,1];
-    let check:number=2;
-    while (true){
-        let numberThree:number=numberOne+numberTwo;
+    let numberOne: number = 0;
+    let numberTwo: number = 1;
+    let arr: number[] = [0, 1];
+    let check: number = 2;
+    while (true) {
+        let numberThree: number = numberOne + numberTwo;
         arr.push(numberThree);
-        numberOne=numberTwo;
-        numberTwo=numberThree;
+        numberOne = numberTwo;
+        numberTwo = numberThree;
         check++;
-        if (check>15){
+        if (check > 9) {
             break;
         }
     }
-    let sum:number=0;
+    let sum: number = 0;
     arr.forEach(function (value) {
-        sum+=value;
+        sum += value;
     })
     console.log(sum);
 }
+
 sumFibonacci();
+
+// Cach 2//
+function fibonacci(n: number) {
+    let f0: number = 0;
+    let f1: number = 1;
+    let fn: number = 1;
+    let i;
+
+    if (n < 0) {
+        return -1;
+    } else if (n == 0 || n == 1) {
+        return n;
+    } else {
+        for (i = 2; i < n; i++) {
+            f0 = f1;
+            f1 = fn;
+            fn = f0 + f1;
+        }
+    }
+    return fn;
+}
+
+for (let i = 0; i < 11; i++) {
+    console.log(fibonacci(i));
+}
+let sum:number=0;
+for (let i = 0; i < 10; i++) {
+    sum += fibonacci(i);
+}
+console.log(sum);
