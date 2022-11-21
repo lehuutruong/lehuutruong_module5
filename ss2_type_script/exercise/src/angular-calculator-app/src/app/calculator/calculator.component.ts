269dd3be0acd28a03eaf5ba068c1a7bce98436fd
TRƯỜNG KHANG <lehuutruongkhang@gmail.com>
2022-11-21 16:25:24 +0700

    C:/Program Files/Git/

--- ss2_type_script/exercise/src/angular-calculator-app/src/app/calculator/calculator.component.ts
+++ ss2_type_script/exercise/src/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  firstNumber: number;
+  secondNumber: number;
+  result: number;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addition() {
+    this.result = parseFloat(String(this.firstNumber)) + parseFloat(String(this.secondNumber));
+  }
+
+  subtraction() {
+    this.result = parseFloat(String(this.firstNumber)) - parseFloat(String(this.secondNumber));
+  }
+
+  multiplication() {
+    this.result = parseFloat(String(this.firstNumber)) * parseFloat(String(this.secondNumber));
+  }
+
+  division() {
+    this.result = parseFloat(String(this.firstNumber)) / parseFloat(String(this.secondNumber));
+  }
 }
