9e0bce4bef7cbcd2723cb85f2179d737376188ce
TRƯỜNG KHANG <lehuutruongkhang@gmail.com>
2022-11-22 17:56:32 +0700

    C:/Program Files/Git/

--- ss2_type_script/exercise/src/angular-calculator-app/src/app/app.module.ts
+++ ss2_type_script/exercise/src/angular-calculator-app/src/app/app.module.ts
@@ -6,13 +6,25 @@ import { CalculatorComponent } from './calculator/calculator.component';
 import {FormsModule} from "@angular/forms";
 import { ColorComponent } from './color/color.component';
 import { HomeCaseStudyComponent } from './home-case-study/home-case-study.component';
+import { ArticleComponent } from './article/article.component';
+import { LikeComponent } from './like/like.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListFacilityComponent } from './list-facility/list-facility.component';
+import { EditFacilityComponent } from './edit-facility/edit-facility.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
     ColorComponent,
-    HomeCaseStudyComponent
+    HomeCaseStudyComponent,
+    ArticleComponent,
+    LikeComponent,
+    FooterComponent,
+    NavbarComponent,
+    ListFacilityComponent,
+    EditFacilityComponent
   ],
   imports: [
     BrowserModule,
