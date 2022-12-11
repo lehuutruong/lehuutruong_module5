0e99f08032fb829f0c767a4eff79089db91e2983
TRƯỜNG KHANG <lehuutruongkhang@gmail.com>
2022-12-11 18:53:36 +0700

    C:/Program Files/Git/

--- heloo_wword/angularDemo/src/service/message.service.ts
+++ heloo_wword/angularDemo/src/service/message.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs/internal/BehaviorSubject';
 import {AngularFireMessaging} from "@angular/fire/messaging";
 
 @Injectable({
@@ -38,6 +38,24 @@ export class MessageService {
       (payload) => {
         console.log('new message received. ', payload);
         this.currentMessage.next(payload);
+        this.showCustomerNotification(payload);
       });
+  }
+
+  showCustomerNotification(payload: any) {
+    let notify_data = payload['notification'];
+    let title = notify_data['title'];
+    let options = {
+      body: notify_data['body'],
+      icon: "./assets/funny.jpg",
+      badge: "./assets/funny.jpg",
+      image: "./assets/ra_dao.jpg",
+    };
+    console.log("new message received", notify_data);
+    let notify: Notification = new Notification(title, options);
+    notify.onclick = event => {
+      event.preventDefault();
+      window.location.href = "https://www.google.com";
+    }
   }
 }
