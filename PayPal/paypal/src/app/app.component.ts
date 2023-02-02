4ca34b950293435f448722cb578934c6d380d1eb
TRƯỜNG KHANG <lehuutruongkhang@gmail.com>
2023-02-02 17:00:54 +0700

    TruongLH

---
+++ PayPal/paypal/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {render} from 'creditcardpayments/creditCardPayments';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'paypal';
+
+  constructor() {
+    render(
+      {
+        id: "#myPaypalButtons",
+        value: "10",
+        currency: "USD",
+        onApprove: (details) => {
+          alert("Thanh toán thành công");
+        }
+      }
+    );
+  }
+}
